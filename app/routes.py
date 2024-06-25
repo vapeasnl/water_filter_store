08b008593f29542b9989503180f91899494f82d8
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2024-06-25 12:34:08 +0200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,14 @@
+from flask import render_template
+from app import app, db
+from app.models import Product
+
+@app.route('/')
+def index():
+    products = Product.query.all()
+    return render_template('index.html', products=products)
+
+@app.route('/product/<int:product_id>')
+def product(product_id):
+    product = Product.query.get_or_404(product_id)
+    return render_template('product.html', product=product)
 
