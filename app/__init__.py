e9e1a5041db8b4bd77141dbe803836d1c2ca4746
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2024-06-25 12:36:34 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 from config import Config
 
 db = SQLAlchemy()
 migrate = Migrate()
+login = LoginManager()
+login.login_view = 'auth.login'
+bcrypt = Bcrypt()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -12,7 +17,13 @@ def create_app(config_class=Config):
 
     db.init_app(app)
     migrate.init_app(app, db)
+    login.init_app(app)
+    bcrypt.init_app(app)
 
-    from app import routes, models
+    from app.routes import main_bp
+    from app.auth.routes import auth_bp
+
+    app.register_blueprint(main_bp)
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
     return app
-
