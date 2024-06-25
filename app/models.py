fb7c5995a429ee20e3d9714a96b81a509a42143f
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2024-06-25 12:36:08 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,4 +1,31 @@
-from app import db
+from datetime import datetime
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db, login
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+    reviews = db.relationship('Review', backref='author', lazy=True)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,4 +40,4 @@ class Product(db.Model):
     stock = db.Column(db.Integer, nullable=False)
     image_url = db.Column(db.String(256))
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
-
+    reviews = db.relationship('Review', backref='product', lazy=True)
