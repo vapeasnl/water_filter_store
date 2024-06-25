71d50185e02f479b033e7a0c1213c9f310ffd6b8
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2024-06-25 12:45:36 +0200

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,44 @@
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, TextAreaField, SubmitField, FloatField, IntegerField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from app.models import User
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
 
 class ReviewForm(FlaskForm):
     body = TextAreaField('Review', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+class MessageForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    body = TextAreaField('Message', validators=[DataRequired()])
+    submit = SubmitField('Send')
+
+class ProductForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    description = TextAreaField('Description', validators=[DataRequired()])
+    price = FloatField('Price', validators=[DataRequired()])
+    stock = IntegerField('Stock', validators=[DataRequired()])
+    image_url = StringField('Image URL', validators=[DataRequired()])
+    submit = SubmitField('Add Product')
