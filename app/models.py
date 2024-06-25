1813ba6a7e0beae198d478cbdd75ce70f52d46a7
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2024-06-25 12:32:43 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,16 @@
+from app import db
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True, nullable=False)
+    products = db.relationship('Product', backref='category', lazy=True)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    stock = db.Column(db.Integer, nullable=False)
+    image_url = db.Column(db.String(256))
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
 
